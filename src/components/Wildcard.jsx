25064ed1e382e040626024ae94bd58b757e919a5
Jason Thompson <jason@jasont.us>
2022-03-28 19:44:41 -0400

    add toggle switch for eliminated teams

--- src/components/Wildcard.jsx
+++ src/components/Wildcard.jsx
@@ -1,11 +1,12 @@
 import { useState, useEffect } from 'react'
-import { Box, Select } from '@mantine/core'
+import { Box, Select, Switch } from '@mantine/core'
 import Chart from './Chart'
 
 const Wildcard = ({ options, standings }) => {
   const [conference, setConference] = useState(null)
   const [data, setData] = useState([])
   const [max, setMax] = useState(140)
+  const [showEliminated, setShowEliminated] = useState(false)
 
   useEffect(() => {
     if (!conference) return
@@ -24,7 +25,8 @@ const Wildcard = ({ options, standings }) => {
     const wildCardTeams = divisions
       .map((division) =>
         division.teamRecords.filter(
-          (team) => team.wildCardRank > 0 && !team.eliminated
+          (team) =>
+            team.wildCardRank > 0 && (!team.eliminated || showEliminated)
         )
       )
       .flat()
@@ -39,9 +41,12 @@ const Wildcard = ({ options, standings }) => {
       gamesPlayed: 82,
     }
 
-    wildCardTeams.splice(2, 0, wildCardSeparator)
+    if (wildCardTeams.length > 2) {
+      wildCardTeams.splice(2, 0, wildCardSeparator)
+    }
+
     setData([...divisionLeaders, { label: 'Wild Card', data: wildCardTeams }])
-  }, [conference, standings])
+  }, [conference, standings, showEliminated])
 
   return (
     <>
@@ -52,25 +57,33 @@ const Wildcard = ({ options, standings }) => {
           data={options}
         />
       </Box>
-      {data &&
-        data.map((chart, idx) => (
-          <Box
-            key={idx}
-            className='chart'
-            my='0'
-            mx='2em'
-            mb={idx === 2 ? '2em' : '0'}
-            sx={{ height: idx === 2 ? '36vh!important' : '16vh!important' }}
-          >
-            <Chart
-              title={chart.label}
-              teams={chart.data}
-              legend={idx === 2}
-              max={max}
-              setMax={setMax}
-            />
-          </Box>
-        ))}
+      {data.map((chart, idx) => (
+        <Box
+          key={idx}
+          className='chart'
+          my='0'
+          mx='2em'
+          mb={idx === 2 ? '2em' : '0'}
+          sx={{ height: idx === 2 ? '36vh!important' : '16vh!important' }}
+        >
+          <Chart
+            title={chart.label}
+            teams={chart.data}
+            legend={idx === 2}
+            max={max}
+            setMax={setMax}
+          />
+        </Box>
+      ))}
+      {conference && (
+        <Box mx='2em'>
+          <Switch
+            label='eliminated teams'
+            checked={showEliminated}
+            onChange={(e) => setShowEliminated(e.currentTarget.checked)}
+          />
+        </Box>
+      )}
     </>
   )
 }
